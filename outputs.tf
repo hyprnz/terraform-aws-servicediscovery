5c00e82017c31beddb11c09caf41fa35c2d4b3ec
Peter Sellars <peter.sellars@hypr.co.nz>
2020-05-28 13:07:25 +1200

    added initial outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "id" {
   description = "The ID of a namespace"
+  value       = aws_service_discovery_private_dns_namespace.this.id
 }
 
 output "arn" {
   description = "The ARN that Amazon Route 53 assigns to the namespace when you create it"
+  value       = aws_service_discovery_private_dns_namespace.this.arn
 }
 
 output "hosted_zone" {
   description = "The ID for the hosted zone that Amazon Route 53 creates when you create a namespace"
+  value       = aws_service_discovery_private_dns_namespace.this.hosted_zone
 }
